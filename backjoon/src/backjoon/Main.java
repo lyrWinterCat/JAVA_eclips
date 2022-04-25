821ec72abf9cfb201a28399f3e85d35eb39d64d1
lyrWinterCat <103623292+lyrWinterCat@users.noreply.github.com>
2022-04-25 17:06:14 +0900

    Test
    hi Test

--- backjoon/src/backjoon/Main.java
+++ backjoon/src/backjoon/Main.java
@@ -4,7 +4,7 @@ package backjoon;
 import java.util.Arrays;
 import java.util.Scanner;
 
-
+//Test Commit
 public class Main {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
