38ea2f3c79001a8c02ef8430dbf043f06d01ddb4
lyrWinterCat <103623292+lyrWinterCat@users.noreply.github.com>
2022-05-27 15:43:33 +0900

    still working

--- backjoon/src/backjoon2941/Main.java
+++ backjoon/src/backjoon2941/Main.java
@@ -8,14 +8,29 @@ public class Main {
 		String words = sc.next();
 		String [] croA = {"c=","c-","dz=","d-","nj","lj","s=","z="};
 		int cnt=0;
+		
+		// 먼저 중복 문자열 제거하기
+		String cWords="";
+		int cCnt=0;
+		for (int i = 0; i < words.length(); i++) {
+			if(!cWords.contains(String.valueOf(words.charAt(i)))) {
+				cWords += String.valueOf(words.charAt(i));
+				cCnt++;
+			}
+		}
+		System.out.println(cCnt);
+		
+		//문자열에 craA 포함 확인
 		for (int i = 0; i < croA.length; i++) {
-			if(words.contains(croA[i])) {
-				words=words.replace(croA[i], "");
+			if(cWords.contains(croA[i])) {
+				cWords=cWords.replace(croA[i], "");
 				cnt++;
-				System.out.println(words);
+				System.out.println(cWords);
 			}
 		}//for
-		cnt+=words.length();
+		
+		System.out.println(cnt);
+		cnt+=cWords.length();
 		System.out.println(cnt);
 		
 	}//main
