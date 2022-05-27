9a19d3a0475ede14f3fafdec3f891b6054745c57
lyrWinterCat <103623292+lyrWinterCat@users.noreply.github.com>
2022-05-27 15:47:03 +0900

    commit

--- backjoon/src/backjoon2941/Main.java
+++ backjoon/src/backjoon2941/Main.java
@@ -10,27 +10,25 @@ public class Main {
 		int cnt=0;
 		
 		// 먼저 중복 문자열 제거하기
-		String cWords="";
-		int cCnt=0;
-		for (int i = 0; i < words.length(); i++) {
-			if(!cWords.contains(String.valueOf(words.charAt(i)))) {
-				cWords += String.valueOf(words.charAt(i));
-				cCnt++;
-			}
-		}
-		System.out.println(cCnt);
+//		String cWords="";
+//		int cCnt=0;
+//		for (int i = 0; i < words.length(); i++) {
+//			if(!cWords.contains(String.valueOf(words.charAt(i)))) {
+//				cWords += String.valueOf(words.charAt(i));
+//				cCnt++;
+//			}
+//		}
+//		System.out.println(cCnt);
 		
 		//문자열에 craA 포함 확인
 		for (int i = 0; i < croA.length; i++) {
-			if(cWords.contains(croA[i])) {
-				cWords=cWords.replace(croA[i], "");
+			if(words.contains(croA[i])) {
+				words=words.replace(croA[i], "");
 				cnt++;
-				System.out.println(cWords);
 			}
 		}//for
-		
-		System.out.println(cnt);
-		cnt+=cWords.length();
+
+		cnt+=words.length();
 		System.out.println(cnt);
 		
 	}//main
