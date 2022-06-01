89295b112dc4041793fced5a5867d02509a36034
lyrWinterCat <103623292+lyrWinterCat@users.noreply.github.com>
2022-06-01 14:28:36 +0900

    hotel

---
+++ backjoon/src/backjoon10250/Main.java
@@ -0,0 +1,19 @@
+package backjoon10250;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int test=sc.nextInt();
+		
+		for (int i = 0; i < test; i++) {
+			int floor=sc.nextInt();
+			int room=sc.nextInt();
+			int guest=sc.nextInt();
+			
+		}
+		
+	}
+
+}
