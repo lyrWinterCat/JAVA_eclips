2080148ecde857dc468c8559f11413169d0a5c91
lyrWinterCat <103623292+lyrWinterCat@users.noreply.github.com>
2022-06-07 15:49:05 +0900

    ?

--- backjoon/src/backjoon4948/Main.java
+++ backjoon/src/backjoon4948/Main.java
@@ -7,24 +7,35 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		while(sc.nextInt()!=0) {
-			int m=sc.nextInt();
-			List<Integer> mList=new ArrayList<>();
-			for (int i = m; i <=2*m; i++) {
-				boolean isCheck=false;
-				for (int j = 2; j <=i; j++) {
-					if(i%j==0) {
-						isCheck=true;
-						return;
+		boolean isZero=false;
+		while(!isZero) {
+			int n = sc.nextInt();
+			
+			if(n==0) {
+				isZero=true;
+			}
+			else if(n==1) {
+				System.out.println(1);
+			}else {
+				int cnt=0;
+				for (int i = n; i <= 2*n; i++) {
+					boolean isCheck=false;
+					for (int j = 2; j < i; j++) {
+						if(i%j==0) {
+							isCheck=true;
+							break;
+						}
+					}//forJ
+					if(!isCheck && i!=1) {
+						cnt++;
 					}
-				}//소수판별반복문
-				if(!isCheck) {
-					mList.add(i);
-				}				
-			}//m반복문
-			System.out.println(mList.size());			
+				}//forI
+				
+				System.out.println(cnt);				
+			}
+			
 		}//while
+			
 	}//main
 
 }
