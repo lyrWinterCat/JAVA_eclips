7a859b27a5292ac01697c7116b165daec013fe20
lyrWinterCat <103623292+lyrWinterCat@users.noreply.github.com>
2022-06-07 17:00:22 +0900

    commit

---
+++ backjoon/src/backjoon9020/Main.java
@@ -0,0 +1,36 @@
+package backjoon9020;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+//		List<Integer> sNum=ArrayList<Integer>(); 
+//		
+//		int[] arrInt=new int[10001];
+//		int i,j;
+//		for(i=0; i<=10000; i++) arrInt[i]=0; arrInt[1]=1;
+//		for(i=2; i<=10000; i++) {
+//			for(j=2; i*j<=10000; j++) {
+//				arrInt[i*j]=1;
+//			}
+//		}
+//		
+//		for (i=2; i<=10000; i++) {
+//			if(arrInt[i]!=1 ) sNum.add(arrInt[i]);
+//		}		
+//		
+//		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int k = 0; k < n; k++) {
+			int num=sc.nextInt();
+			//입력한 수보다 작은 소수들
+			// 소수들의 합이 num이 되면 됨
+			
+		}
+		
+	}
+
+}
