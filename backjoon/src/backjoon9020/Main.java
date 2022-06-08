19c0e0446657940dda479c3fc3aac21d7c24525b
lyrWinterCat <103623292+lyrWinterCat@users.noreply.github.com>
2022-06-08 15:26:05 +0900

    solving

--- backjoon/src/backjoon9020/Main.java
+++ backjoon/src/backjoon9020/Main.java
@@ -1,5 +1,6 @@
 package backjoon9020;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -25,11 +26,39 @@ public class Main {
 		int n = sc.nextInt();
 		
 		for (int k = 0; k < n; k++) {
+			List<Integer> sNum=new ArrayList<>();
 			int num=sc.nextInt();
 			//입력한 수보다 작은 소수들
 			// 소수들의 합이 num이 되면 됨
+			for (int i = 2; i < num; i++) {
+				boolean isCheck=false;
+				for (int j = 2; j < i; j++) {
+					if(i%j==0) {
+						isCheck=true;
+					}
+				}
+				if(!isCheck) {
+					sNum.add(i);
+				}
+			}//소수for
+			System.out.println(sNum); 
+			int a=0;
+			int b=0;
 			
-		}
+			for (int i = 0; i < sNum.size(); i++) {
+				if(sNum.contains(num-sNum.get(i))){
+					a=sNum.get(i);
+					b=num-a;
+				}
+			}
+			System.out.println(a+" "+b);
+			
+			//...?? 두 소수가 가장 작은걸...어케만들지?
+			
+			
+			
+			
+		}//for
 		
 	}
 
