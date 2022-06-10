841620fa98fdf5ca020b1abad320eddffb913f9a
lyrWinterCat <103623292+lyrWinterCat@users.noreply.github.com>
2022-06-10 12:58:19 +0900

    solution

--- backjoon/src/backjoon9020/Main.java
+++ backjoon/src/backjoon9020/Main.java
@@ -27,42 +27,21 @@ public class Main {
 				if(arrInt[i]!=1) sNum.add(i);
 			}	
 			
-			System.out.println(sNum);
+//			System.out.println(sNum);
 			
 			//파티션 구하기
-			int a=0;
-			int b=0;
+			int a=num/2;
+			int b=num/2;
 			
-			if(num==4) {
-				a=2;
-				b=2;
-			} else {
-				//소수 개수가 짝수일때
-				if(sNum.size()%2==0) {
-					for (int m = sNum.size()/2-1; m <sNum.size(); m++) {
-						if(sNum.contains(sNum.get(m))&&sNum.contains(num-sNum.get(m))){
-							a=sNum.get(m-1);
-							b=num-a;
-						}
-					}					
-				}else {
-					// 소수 개수가 홀수일때
-					for (int m = sNum.size()/2-1; m <sNum.size(); m++) {
-						if(sNum.contains(sNum.get(m))&&sNum.contains(num-sNum.get(m))){
-							a=sNum.get(m);
-							b=num-a;
-						}
-					}
-				}				
+			while(a+b==num) {
+				if(sNum.contains(a)&&sNum.contains(b)) {
+					break;
+				}
+				a--;
+				b++;
 			}
 			
-			if(a>b) {
-				System.out.println(b+" "+a);				
-			} else if(b>a) {
-				System.out.println(a+" "+b);
-			} else if(b==a){
-				System.out.println(b+" "+a);
-			}
+			System.out.println(a+" "+b);
 			
 		}//for
 	}
